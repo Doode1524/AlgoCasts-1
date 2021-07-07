6b5322ac1ab2a9ec8bd47adbd5d38d1325d2f480
joey <doode1524@gmail.com>
2021-07-07 17:00:12 -0400

    test

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,28 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    const arr = str.split('')
-    arr.reverse()
-    return arr.join('')
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed
+    }, '')
 }
+
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// function reverse(str) {
+//     const arr = str.split('')
+//     arr.reverse()
+//     return arr.join('')
+// }
 
 module.exports = reverse;
